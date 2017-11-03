7f2b11ffcac5cfb10c35603da1ced61b8edff010
Your-Full-NameJohann <jSchuster>
2017-11-03 15:06:07 -0700

    integrated/map/over/projects

--- src/components/addProjects.js
+++ src/components/addProjects.js
@@ -1,111 +1,88 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import GitHub from 'react-icons/lib/fa/github'
 import View from 'react-icons/lib/fa/laptop'
 
 function AddProjects() {
+
+    const additional = [
+        {
+            id: 0,
+            name: 'React Recipe App',
+            notes: 'This is a simple recipe application built with React and SASS. A user can add recipes and store, edit, and delete them as needed.I strived to make clear distinctions between Container and UI Components and made use of local storage to store each recipe, and finished it up with a simple clean, easy to use design.', 
+            technologies: ['html', 'css/sass', 'react'], 
+            view: 'https://codepen.io/j-schuster/full/qPOMmO/',
+            code: 'https://codepen.io/j-schuster/full/qPOMmO/',       
+            tId: 'z'          
+        },
+         {
+            id: 1,
+            name: 'Simon Game',
+            notes: 'Part of the advanced front-end projects for the Freecodecamp curriculum. This is a digital replica of the famous 80’s memory game called Simon. The game sets a sequence that the user must follow, as the game progresses the sequence becomes harder and harder. Unless the user fails within 13 rounds, he/ she wins. I chose to write it using Vanilla JavaScript specifically  to avoid using an external library like jQuery and made use of the Module Design Pattern to structure the application.',                     
+            technologies: ['html', 'css', 'javascript'], 
+            view: 'https://js-simone-game.herokuapp.com/',
+            code: 'https://github.com/j-schuster/javascript-simon-game',       
+            tId: 'a'          
+        },
+         {
+            id: 2,
+            name: 'Pomodoro Clock',
+            notes: 'Part of the advanced front-end projects for the Freecodecamp curriculum.The pomodoro clock is a useful tool to manage work and rest intervals. The application allows the user to set a time period for a work sesion and the same for a break session, the app will then run until the user stops it. Once again I used Vanilla JavaScript to build in the functionality. The app makes use of encapsulation, I explored with Immediately Invoked Functions and using the this keyword.',                     
+            technologies: ['html', 'css', 'javascript'], 
+            view: 'https://js-pomodoro-clock.herokuapp.com/',
+            code: 'https://github.com/j-schuster/pomodoro-clock',       
+            tId: 'b'          
+        },
+
+         {
+            id: 3,
+            name: 'Tic Tac Toe',
+            notes: 'A version of the popular game with one extra feature. I incorporated the famous MiniMax algorithm to make the game unbeatable.  The user can at most tie against the computer.  The algorithm allows the computer to see all possible configurations of the the future state of the game, each configuration returns a score and the computer plays the next move according to the highest score.  I used Vanilla JavaScript to build this game. ',                     
+            technologies: ['html', 'css', 'javascript', 'jQuery'], 
+            view: 'https://codepen.io/j-schuster/pen/brvGRg',
+            code: 'https://github.com/j-schuster/js-tic-tac',       
+            tId: 'c'          
+        },
+
+         {
+            id: 4,
+            name: 'JS Calculator',
+            notes: 'Another one of the advanced front-end projects for Freecodecamp. It is a simple calculator that I built using Vanilla JavaScript. The application has all the functionality of a simple calculator  and returns the correct evaluations of any sequence of mathematical tasks. The app makes use of two kinds of memories to make this possible, current and stored. When the user inputs a sequence, it gets stored in the stored memory, this way a string of calculations can be accurately determined.',                     
+            technologies: ['html', 'css', 'javascript'], 
+            view: 'https://js-claculator.herokuapp.com/',
+            code: 'https://github.com/j-schuster/js-calculator',       
+            tId: 'd'          
+        },
+
+    ]
+
 	return(
 		    <div className="section-additional-projects">
             <div className="container">
                 <div className="other-projects-header col-md-6">
                     <h3>OTHER PROJECTS</h3>
                 </div>
-                <div className="other-projects-info col-md-6">
-                    <h3>React Recipe App</h3>
-                
-                    <a href="https://codepen.io/j-schuster/full/qPOMmO/" target="_blank" rel="noopener noreferrer"><View className="icon-ap"/></a>
-                    <p>This is a simple recipe application built with React and SASS.
-                     A user can add recipes and store/ edit/ delete them as needed.
-                      I strived to make clear distinctions between Container and UI 
-                      Components and made use of local storage to store each recipe, 
-                      and finished it up with a simple clean, easy to use design. 
-                     </p>
-                     <li className="technology">html</li>
-                     <li className="technology">css/sass</li>
-                     <li className="technology">react.js</li>
-                </div>
 
-
-
-
-                  <div className="other-projects-header col-md-6">
-                </div>
-                <div className="other-projects-info col-md-6">
-                    <h3>Simon Game</h3>
-                    <a href="https://github.com/j-schuster/javascript-simon-game" target="_blank" rel="noopener noreferrer"><GitHub className="icon-ap"/></a>
-                    <a href="https://js-simone-game.herokuapp.com/" target="_blank"  rel="noopener noreferrer"><View className="icon-ap"/></a>
-                    <p>Part of the advanced front-end projects for the Freecodecamp curriculum.
-                     This is a digital replica of the famous 80’s memory game called Simon. 
-                     The game sets a sequence that the user must follow, as the game progresses the sequence becomes harder and harder.
-                      Unless the user fails within 13 rounds, he/ she wins. I chose to write it using Vanilla JavaScript specifically 
-                      to avoid using an external library like jQuery and made use of the Module Design Pattern to structure the application.
-                     </p>
-                     <li className="technology">html</li>
-                     <li className="technology">css</li>
-                     <li className="technology">javascript</li>
-                </div>
-
-
-                  <div className="other-projects-header col-md-6">
-                   
-                </div>
-                <div className="other-projects-info col-md-6">
-                    <h3>Pomodoro Clock</h3>
-                    <a href="https://github.com/j-schuster/pomodoro-clock" target="_blank" rel="noopener noreferrer"><GitHub className="icon-ap"/></a>
-                    <a href="https://js-pomodoro-clock.herokuapp.com/" target="_blank" rel="noopener noreferrer"><View className="icon-ap"/></a>
-                    <p>Part of the advanced front-end projects for the Freecodecamp curriculum.
-                     The pomodoro clock is a useful tool to manage work and rest intervals.
-                      The application allows the user to set a time period for a work sesion and the same for a break session, 
-                      the app will then run until the user stops it. Once again I used Vanilla JavaScript to build in the functionality. 
-                      The app makes use of encapsulation, I explored with Immediately Invoked Functions and using the this keyword, 
-                      call and apply to bind functions. 
-                     </p>
-                     <li className="technology">html</li>
-                     <li className="technology">css</li>
-                     <li className="technology">javascript</li>
-                </div>
-
-                  <div className="other-projects-header col-md-6">
-             
-                </div>
-                <div className="other-projects-info col-md-6">
-                    <h3>Tic Tac Toe</h3>
-                    <a href="https://github.com/j-schuster/js-tic-tac" target="_blank" rel="noopener noreferrer"><GitHub className="icon-ap"/></a>
-                    <a href="https://codepen.io/j-schuster/pen/brvGRg" target="_blank" rel="noopener noreferrer"><View className="icon-ap"/></a>
-                    <p>A version of the popular game with one extra feature.
-                     I incorporated the famous MiniMax algorithm to make the game unbeatable. 
-                     The user can at most tie against the computer. 
-                     The algorithm allows the computer to see all possible configurations of the the future state of the game
-                     , each configuration returns a score and the computer plays the next move according to the highest score. 
-                     I used Vanilla JavaScript to build this game. 
-                     </p>
-                     <li className="technology">html</li>
-                     <li className="technology">css</li>
-                     <li className="technology">jQuery</li>
-                     <li className="technology">javascript</li>
-                </div>
-
-                  <div className="other-projects-header col-md-6">
-                    
-                </div>
-                <div className="other-projects-info col-md-6">
-                    <h3>JS CALCULATOR</h3>
-                    <a href="https://github.com/j-schuster/js-calculator" target="_blank" rel="noopener noreferrer"><GitHub className="icon-ap"/></a>
-                    <a href="https://js-claculator.herokuapp.com/" target="_blank" rel="noopener noreferrer"><View className="icon-ap"/></a>
-                    <p>Another one of the advanced front-end projects for Freecodecamp. 
-                    It is a simple calculator that I built using Vanilla JavaScript.
-                     The application has all the functionality of a simple calculator 
-                     and returns the correct evaluations of any sequence of mathematical tasks. 
-                     The app makes use of two kinds of memories to make this possible, current and stored.
-                      When the user inputs a sequence, it gets stored in the stored memory,
-                       this way a string of calculations can be accurately determined.
-                     </p>
-                     <li className="technology">html</li>
-                     <li className="technology">css</li>
-                     <li className="technology">javascript</li>
-                </div>
+                {additional.map((project) => (
+                    <li key={project.id}> 
+                         <div className="other-projects-info col-md-6">
+                             <h3>{project.name}</h3>                
+                                <Link to={project.view} target="_blank"><View className="icon-ap"/></Link>
+                                <Link to={project.code} target="_blank"><GitHub className="icon-ap"/></Link> 
+                           <p>{project.notes}</p>
+                         {project.technologies.map((i) => (
+                        <ul key={i}>
+                         <li>{i}</li>
+                         </ul>
+                         ))}
+                    </div>
+                </li>
+                ))}
             </div>
         </div>
 		);
 }
 
 export default AddProjects;
+
+
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,19 +1,94 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
 import GitHub from 'react-icons/lib/fa/github'
 import View from 'react-icons/lib/fa/laptop'
+import readable from '../photo/mac5.png'
+import myReads from '../photo/mac.png'
+import portfolio from '../photo/mac9.png'
 
 
-class Projects extends Component {
-  render() {
+function Projects() {
+
+  const projects = [
+    {
+      id: 0,
+      title: 'RECENT PROJECTS',
+      name: 'Readable',
+      image: portfolio,
+      view: 'https://github.com/j-schuster/readable',
+      code: 'https://github.com/j-schuster/readable',
+      notes: 'Readable is a content sharing web app built with React/ Redux. It is the final project for Udacity’s React Nanodegree. The app incorporates state management using Redux, CRUD functionality using REST principles and React Router for link navigation.', 
+    },
+       {
+      id: 1,
+      title: '\u200B',
+      name: 'My Reads',
+      image: portfolio,
+      view: 'https://github.com/j-schuster/my-reads-bookshelf-app',
+      code: 'https://github.com/j-schuster/my-reads-bookshelf-app',
+      notes: 'My Reads is book reading tracking application. The user is able to search for books from a database and categorize them into shelves, either Read, Currently reading or Want To Read.', 
+    },
+     {
+      id: 2,
+      title: '\u200B',
+      name: 'Personal Website V2',
+      image: portfolio,
+      view: 'www.jschuster.me',
+      code: 'https://github.com/j-schuster/portfolio-site',
+      notes: 'Designed and developed with a conscious effort to maintain a balance between simplicity, sophistication and elegance.',
+    },
+  ]
+
+  
     return (
           <div id="projects" className="section-projects">
-            <div className="row container-fluid">
-              <div className="col-md-3 col-sm-12 col-xs-12 projects-left">
+            <div className="row container-fluid">      
+                {projects.map((project) => (
+                  <li key={project.id}>
+                    <div className="col-md-3 col-sm-12 col-xs-12 projects-left"><h3>{project.title}</h3></div>
+                      <div className="col-md-7 col-sm-12 col-xs-12 projects-image-one">
+                        <div className="projects-one" 
+                         style={{ 
+                                  height: '30em',
+                                  backgroundImage: `url(${project.image})`,
+                                  backgroundSize: 'contain',
+                                  backgroundRepeat: 'no-repeat',
+                                  margin: '0 auto'
+                               }}></div>
+                         </div>
+                      <div className="col-md-2 col-sm-12 col-xs-12 projects-text">
+                        <h4>{project.name}</h4>
+                           <p>{project.notes}</p>  
+                            <Link to={project.code} target="_blank"><GitHub  className="icon-gh"/></Link>
+                          <Link to={project.view} target="_blank"><View className="icon-gh"/></Link>
+                      </div>
+                  </li>
+                  ))}
+             </div>
+          </div>
+     );
+   
+}
+
+export default Projects;
+
+
+/*
+ <div className="col-md-3 col-sm-12 col-xs-12 projects-left">
                 <h3>RECENT PROJECTS</h3>
-              </div>
+               </div>
+
+
+
+
+                <div className="row container-fluid">
+             
+            <div className="col-md-3 col-sm-12 col-xs-12 projects-left row"></div>
+
               <div className="col-md-7 col-sm-12 col-xs-12 projects-image-one">
                 <div className="projects-one"></div>
               </div>
+
               <div className="col-md-2 col-sm-12 col-xs-12 projects-text">
                 <h4>Readable</h4>
                     <p>Readable is a content sharing web app built with React/ Redux.
@@ -23,9 +98,9 @@ class Projects extends Component {
                     <a><View className="icon-gh"/></a>
               </div>
           </div>
+
           <div className="row container-fluid">
-            <div className="col-md-3 col-sm-12 col-xs-12 projects-left">
-          </div>
+           <div className="col-md-3 col-sm-12 col-xs-12 projects-left row"></div>
           <div className="col-md-7 col-sm-12 col-xs-12 projects-image-one">
             <div className="projects-two"></div>
           </div>
@@ -38,12 +113,15 @@ class Projects extends Component {
                      <a><View className="icon-gh"/></a>
             </div>
             </div>
+
+
             <div className="row container-fluid">
-               <div className="col-md-3 projects-left">
-            </div>
+             <div className="col-md-3 col-sm-12 col-xs-12 projects-left row"></div>
             <div className="col-md-7 projects-image-one">
                 <div className="projects-three"></div>
             </div>
+
+
                <div className="col-md-2 projects-text">
                   <h4>Personal Website V2</h4>
                     <p>Second iteration of my personal website.
@@ -52,10 +130,6 @@ class Projects extends Component {
                      <a href="https://github.com/j-schuster/portfolio-site" target="_blank"  rel="noopener noreferrer"><GitHub  className="icon-gh"/></a>
                      <a><View className="icon-gh"/></a>
                   </div>
+               
               </div>
-           </div>
-     );
-   }
-}
-
-export default Projects;
+*/
